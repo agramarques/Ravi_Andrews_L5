b75e562278096c566eb56e284ce7ba338a498f7f
agramarques <agramarques@gmail.com>
2017-02-02 11:48:10 -0300

    final

--- src/hotel/Recepcao.java
+++ src/hotel/Recepcao.java
@@ -1,18 +1,17 @@
 package hotel;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Recepcao {
 
-	private HashMap<String, Estadias> estadias;
+	private HashMap<String, Estadia> estadias = new HashMap<String, Estadia>();
 	
 	public Recepcao() {
-		estadias = new HashMap();
-	
+
 	}
 	
 	public void CheckIn(String nome, String tipo, int idade, double valor, int dias){
-		Animal novoAnimal = new Animal(tipo, idade, nome);
+		Animal novoAnimal = new Animal(nome, tipo, idade);
 		Estadia novaEstadia = new Estadia(novoAnimal, valor, dias);
 		estadias.put(nome, novaEstadia);
 	}
@@ -20,5 +19,25 @@ public class Recepcao {
 	public void CheckOut(String nome){
 		estadias.remove(nome);
 	}
+	
+	public int getNumDeHospedes(){
+		return estadias.size();
+	}
+	
+	public double getLucroTotal(){
+		double lucro = 0;
+		for(String n:estadias.keySet()){
+			Estadia i = estadias.get(n);
+			lucro += i.getValor() * i.getDias();
+		}
+		return lucro;
+	}
+
+	@Override
+	public String toString() {
+		return "Recepcao [estadias=" + estadias + "]";
+	}
+
+	
 	
 }
